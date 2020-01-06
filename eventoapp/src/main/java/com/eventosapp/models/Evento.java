2f051be5337cc2c3da2ee995cbb7d4335629e57b
Jackson <jacksonga@hotmail.com>
2020-01-06 17:10:10 -0300

    Finalizando a aula 3

--- eventoapp/src/main/java/com/eventosapp/models/Evento.java
+++ eventoapp/src/main/java/com/eventosapp/models/Evento.java
@@ -1,6 +1,20 @@
 package com.eventosapp.models;
 
-public class Evento {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long codigo;
 
 	private String nome;
 	private String local;
@@ -31,6 +45,11 @@ public class Evento {
 	public void setHorario(String horario) {
 		this.horario = horario;
 	}
-	
-	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+		
 }
