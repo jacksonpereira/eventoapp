e580d0fbdd4019d543be794122f68c8de196212f
Jackson Pereira da Silva <jacksonga@hotmail.com>
2020-02-04 01:39:34 -0300

    Aula 7 terminada

--- eventoapp/src/main/java/com/eventosapp/models/Evento.java
+++ eventoapp/src/main/java/com/eventosapp/models/Evento.java
@@ -1,23 +1,6 @@
 package com.eventosapp.models;
 
 import java.io.Serializable;
-<<<<<<< HEAD
-=======
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class Evento implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private long codigo;
->>>>>>> 2f051be5337cc2c3da2ee995cbb7d4335629e57b
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -68,9 +51,4 @@ public class Evento implements Serializable{
 	public void setCodigo(long codigo) {
 		this.codigo = codigo;
 	}
-<<<<<<< HEAD
-	
-=======
-		
->>>>>>> 2f051be5337cc2c3da2ee995cbb7d4335629e57b
 }
